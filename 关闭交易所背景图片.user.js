40f213404ec8255a15539a71667627676eac574a
gangbaruby <40230962+gangbaRuby@users.noreply.github.com>
2024-10-13 16:50:05 +0800

    create

---
+++ 关闭交易所背景图片.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         关闭交易所背景图片
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Hide Market Background Image with CSS
+// @author       Rabbit House
+// @match        https://www.simcompanies.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Add a style tag to the document head to hide the images with the specified class
+    const style = document.createElement('style');
+    style.innerHTML = 'img.css-1io2n3s { display: none !important; }';
+    document.head.appendChild(style);
+})();
