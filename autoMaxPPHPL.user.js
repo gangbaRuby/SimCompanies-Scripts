d0559ffba66820f2a9505ea75d810dd42c762431
gangbaruby <40230962+gangbaRuby@users.noreply.github.com>
2025-08-30 14:22:54 +0800

    同步最新计算公式 l = Math.max(.9, s / 2 + .5)

--- autoMaxPPHPL.user.js
+++ autoMaxPPHPL.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         自动计算最大时利润
 // @namespace    https://github.com/gangbaRuby
-// @version      1.17.2
+// @version      1.18.0
 // @license      AGPL-3.0
 // @description  在商店计算自动计算最大时利润，在合同、交易所展示最大时利润
 // @author       Rabbit House
@@ -38,7 +38,7 @@
     const qpt = (e, t, r, n, a = 1) => (a * ((n - t) * 3600) - r) / (e + r);
     const Bpt = (e, t, r, n, a, o) => {
         const g = zn.RETAIL_ADJUSTMENT[e] ?? 1;
-        const s = Math.min(Math.max(2 - n, 0), 2), l = Math.max(0.75, s / 2 + 0.5), c = r / 12;
+        const s = Math.min(Math.max(2 - n, 0), 2), l = Math.max(0.9, s / 2 + 0.5), c = r / 12;
         const d = zn.PROFIT_PER_BUILDING_LEVEL * (t.buildingLevelsNeededPerUnitPerHour * t.modeledUnitsSoldAnHour + 1) * g * (s / 2 * (1 + c * zn.RETAIL_MODELING_QUALITY_WEIGHT)) + (t.modeledStoreWages ?? 0);
         // console.log(`t.buildingLevelsNeededPerUnitPerHour:${t.buildingLevelsNeededPerUnitPerHour}, t.modeledUnitsSoldAnHour:${t.modeledUnitsSoldAnHour}, t.modeledStoreWages:${t.modeledStoreWages} , s:${s} , c:${c}, g:${g}`)
         const h = t.modeledUnitsSoldAnHour * l;
@@ -1693,7 +1693,7 @@
         const Bpt = (e, t, r, n, a, o) => {
             const g = zn.RETAIL_ADJUSTMENT[e] ?? 1;
             const s = Math.min(Math.max(2 - n, 0), 2),
-                  l = Math.max(0.75, s / 2 + 0.5),
+                  l = Math.max(0.9, s / 2 + 0.5),
                   c = r / 12;
             const d = zn.PROFIT_PER_BUILDING_LEVEL *
                 (t.buildingLevelsNeededPerUnitPerHour * t.modeledUnitsSoldAnHour + 1) *
@@ -2007,7 +2007,7 @@
             const Bpt = (e, t, r, n, a, o) => {
                 const g = zn.RETAIL_ADJUSTMENT[e] ?? 1;
                 const s = Math.min(Math.max(2 - n, 0), 2),
-                      l = Math.max(0.75, s / 2 + 0.5),
+                      l = Math.max(0.9, s / 2 + 0.5),
                       c = r / 12;
                 const d = zn.PROFIT_PER_BUILDING_LEVEL *
                     (t.buildingLevelsNeededPerUnitPerHour * t.modeledUnitsSoldAnHour + 1) *
@@ -3751,7 +3751,7 @@
                 const Bpt = (e, t, r, n, a, o) => {
                     const g = zn.RETAIL_ADJUSTMENT[e] ?? 1;
                     const s = Math.min(Math.max(2 - n, 0), 2),
-                          l = Math.max(0.75, s / 2 + 0.5),
+                          l = Math.max(0.9, s / 2 + 0.5),
                           c = r / 12;
                     const d = zn.PROFIT_PER_BUILDING_LEVEL *
                         (t.buildingLevelsNeededPerUnitPerHour * t.modeledUnitsSoldAnHour + 1) *
@@ -3965,7 +3965,7 @@
         const localVersion = GM_info.script.version;
         const scriptUrl = 'https://simcompanies-scripts.pages.dev/autoMaxPPHPL.user.js?t=' + Date.now();
         const downloadUrl = 'https://simcompanies-scripts.pages.dev/autoMaxPPHPL.user.js';
-        // @changelog    增加当前领域天气加成显示
+        // @changelog    同步最新计算公式 l = Math.max(.9, s / 2 + .5)
 
         fetch(scriptUrl)
             .then(res => {
